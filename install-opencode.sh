e64acd439e4ad3a2487373165fc1a59a02eca209
Min RK <benjaminrk@gmail.com>
2026-09-22 09:12:32 -0700

    add tqdm, update opencode

--- install-opencode.sh
+++ install-opencode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-OPENCODE_VERSION=v1.18.17
+OPENCODE_VERSION=v1.18.32
 # hardcode env for Dockerfile
 platform="linux-x64"
 curl -s -L https://github.com/anomalyco/opencode/releases/download/${OPENCODE_VERSION}/opencode-${platform}.tar.gz | tar -xzv -C "$CONDA_PREFIX/bin"
